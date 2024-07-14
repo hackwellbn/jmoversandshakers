cf098bb6d844baa0a2c16430915bc3f7b3a9d106
hackwellbn <oburasusan1@.com>
2024-07-14 20:52:01 +0300

    pending commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTiktok, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <section className="Links">
+        <div className="FooterLinks">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faFacebook} className="icon" />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} className="icon" />
+          </a>
+          <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTiktok} className="icon" />
+          </a>
+          <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faWhatsapp} className="icon" />
+          </a>
+        </div>
+      </section>
       <p>&copy; 2024 Movers & Shakers. All rights reserved.</p>
     </footer>
   );
