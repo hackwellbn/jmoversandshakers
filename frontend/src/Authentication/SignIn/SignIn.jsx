92a41d52285db0fa368d8cd2750347b6e1fcffd0
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2024-07-23 18:19:15 +0300

    Update SignIn.jsx

--- frontend/src/Authentication/SignIn/SignIn.jsx
+++ frontend/src/Authentication/SignIn/SignIn.jsx
@@ -16,7 +16,7 @@ const SignIn = () => {
 
   const create = async (user) => {
     try {
-      const response = await fetch('http://localhost:3000/signIn', {
+      const response = await fetch('https://jmoversandshakers.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
