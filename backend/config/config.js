794a29834d9980d5f79041708d480af663182bd7
hackwellbn <oburasusan1@.com>
2024-07-24 12:44:45 +0300

    pending commits

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'your-default-jwt-secret',
+  host: process.env.HOST || 'localhost',
 };
 
 export default config;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config(); // Ensure .env is loaded
+
 import app from './app.js';
 import connectDB from './db/connectDB.js';
 import config from './config/config.js';
 
 const PORT = config.port;
+const HOST = config.host;
+
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
